6a29419bff732436f552d2cd4450142a9a3ac415
Cadence Ember <cloudrac3r@vivaldi.net>
2020-05-18 00:13:08 +1200

    FIx infinite loop

--- src/site/api/404.js
+++ src/site/api/404.js
@@ -2,9 +2,9 @@ const {render} = require("pinski/plugins")
 const constants = require("../../lib/constants")
 
 module.exports = [
-	{route: "/404", methods: ["GET", "POST", "PATCH", "DELETE"], code: async ({url}) => {
+	{route: "/404", methods: ["*"], code: async ({url}) => {
 		const path = url.searchParams.get("pathname")
-		const couldBeUsername = path.match(`^/${constants.external.username_regex}$`)
+		const couldBeUsername = path && path.match(`^/${constants.external.username_regex}(?:/channel)?$`)
 		return render(404, "pug/404.pug", {path, couldBeUsername})
 	}}
 ]

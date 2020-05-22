5237544d55a3d2dd1a0ed762824cac06501b27ff
Stormking <sturmspitze@googlemail.com>
2020-05-22 10:14:49 +0200

    knex pool settings

--- src/server/repository/base.js
+++ src/server/repository/base.js
@@ -2,7 +2,12 @@ const knex = require('knex');
 
 const connection = knex({
 	client: 'pg',
-	connection: process.env.DATABASE_URL
+	connection: process.env.DATABASE_URL,
+	pool: {
+		min: 0,
+		max: 100,
+		log: (message, logLevel) => console.log(`${logLevel}: ${message}`)
+	}
 })
 
 module.exports = connection;

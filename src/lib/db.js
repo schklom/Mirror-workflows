0d251779f2f52d256cd6485dcf34c1c44f1bf96e
Kyle Anthony Williams <kyle.anthony.williams2@gmail.com>
2020-11-03 11:14:41 -0500

    Changed the journal mode of the SQLite DB to WAL.
    https://github.com/JoshuaWise/better-sqlite3/issues/262#issuecomment-549872386
    
    https://www.sqlite.org/wal.html
    
    Just do it. It should near instanly make Bibliogram faster.

--- src/lib/db.js
+++ src/lib/db.js
@@ -5,4 +5,5 @@ const fs = require("fs")
 const dir = pj(__dirname, "../../db")
 fs.mkdirSync(pj(dir, "backups"), {recursive: true})
 const db = new sqlite(pj(dir, "bibliogram.db"))
+db.pragma("journal_mode = WAL")
 module.exports = db

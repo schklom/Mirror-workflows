e5c0c7707c4b839170da6265ed3d8ace02661d36
Cadence Ember <cadence@disroot.org>
2020-11-06 03:04:07 +1300

    Tweaks to submitted language files

--- src/lang/index.js
+++ src/lang/index.js
@@ -7,7 +7,7 @@ class Lang {
 
 		this.backing.set("base", require("./base"))
 
-		for (const code of ["en", "en-us", "es", "fa", "fr", "gl", "it", "ru", "bg"]) {
+		for (const code of ["bg", "en", "en-us", "es", "fa", "fr", "gl", "it", "ru"]) {
 			// Assign lang
 			const data = require(`./${code}`)
 			this.backing.set(code, data)

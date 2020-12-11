65254f5db47ce3fb8ce1bf9c4bbda2cd6f977cf8
Andrew Dolgov <noreply@fakecake.org>
2020-12-11 09:48:34 +0300

    - move sphinx plugin to a separate repo - regenerate config checks without sphinx-related variables

--- utils/regen_config_checks.sh
+++ utils/regen_config_checks.sh
@@ -5,7 +5,7 @@ DESTINATION="include/sanity_config.php"
 echo "<?php # This file has been generated at: " `date` > $DESTINATION
 
 echo -n "define('GENERATED_CONFIG_CHECK', " >> $DESTINATION
-grep CONFIG_VERSION config.php-dist | awk -F ' |)' '{ print $2 }' | xargs echo -n >> $DESTINATION
+grep CONFIG_VERSION config.php-dist | awk -F ' |\)' '{ print $2 }' | xargs echo -n >> $DESTINATION
 echo ");" >> $DESTINATION
 
 echo -n "\$required_defines = array( " >> $DESTINATION

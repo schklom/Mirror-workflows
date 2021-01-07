0868ff9d6495151a7cc25db541915943aefcd740
Andrew Dolgov <noreply@fakecake.org>
2021-01-07 11:18:02 +0300

    auth_remote: use empty() instead of isset() while checking headers

--- plugins/auth_remote/init.php
+++ plugins/auth_remote/init.php
@@ -44,7 +44,7 @@ class Auth_Remote extends Plugin implements IAuthModule {
 		$try_login = "";
 
 		foreach (["REMOTE_USER", "HTTP_REMOTE_USER", "REDIRECT_REMOTE_USER", "PHP_AUTH_USER"] as $hdr) {
-			if (isset($_SERVER[$hdr])) {
+			if (!empty($_SERVER[$hdr])) {
 				$try_login = $_SERVER[$hdr];
 				break;
 			}

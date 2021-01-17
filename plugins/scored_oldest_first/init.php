c82457e534a573d5838ccecd9ab94cf7c5b05cc2
Andrew Dolgov <noreply@fakecake.org>
2021-01-17 10:47:37 +0300

    add plugins/scored_oldest_first

---
+++ plugins/scored_oldest_first/init.php
@@ -0,0 +1,35 @@
+<?php
+class Scored_Oldest_First extends Plugin {
+
+	function init($host) {
+		$host->add_hook($host::HOOK_HEADLINES_CUSTOM_SORT_MAP, $this);
+		$host->add_hook($host::HOOK_HEADLINES_CUSTOM_SORT_OVERRIDE, $this);
+	}
+
+	function hook_headlines_custom_sort_map() {
+		return [
+			"dates_reverse_scored" => "Oldest first (legacy)"
+		];
+	}
+
+	function hook_headlines_custom_sort_override($order) {
+		if ($order == "dates_reverse_scored") {
+			return [ "score DESC, updated", true ];
+		} else {
+			return [ "", false ];
+		}
+	}
+
+	function about() {
+		return array(1.0,
+			"Consider article score while sorting by oldest first",
+			"fox",
+			false,
+			"");
+	}
+
+	function api_version() {
+		return 2;
+	}
+
+}

schklom/Mirror-workflows

commit e1883b2d9e24acf492a2218c6664ff19bf7818c0

Set default Invidious instance to invidious.io

src/lib/constants.js
@@ -108,7 +108,7 @@ let constants = {
 
 	default_user_settings: {
 		language: "en",
-		rewrite_youtube: "invidio.us",
+		rewrite_youtube: "invidious.io",
 		rewrite_twitter: "nitter.net"
 	},
 

commit c050f6424ab6c67817bddd7432d7c00ec7561aa8

use redirect.invidious.io for youtube redirection
fix regression introduced by commit e1883b2d9e24acf492a2218c6664ff19bf7818c0
invidious.io is not the redirect website for invidious, but the homepage
of Invidious (project)

src/lib/constants.js
@@ -108,7 +108,7 @@ let constants = {
 
 	default_user_settings: {
 		language: "en",
-		rewrite_youtube: "invidious.io",
+		rewrite_youtube: "redirect.invidious.io",
 		rewrite_twitter: "nitter.net"
 	},
 
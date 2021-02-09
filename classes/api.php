2f8efab275f9c62c18fa4a5dd06273b4809480ee
Andrew Dolgov <noreply@fakecake.org>
2021-02-09 12:04:59 +0300

    api: one more php8 warning

--- classes/api.php
+++ classes/api.php
@@ -771,8 +771,7 @@ class API extends Handler {
 
 					$headline_row["labels"] = $labels;
 
-					$headline_row["feed_title"] = $line["feed_title"] ? $line["feed_title"] :
-						$feed_title;
+					$headline_row["feed_title"] = isset($line["feed_title"]) ? $line["feed_title"] : $feed_title;
 
 					$headline_row["comments_count"] = (int)$line["num_comments"];
 					$headline_row["comments_link"] = $line["comments"];

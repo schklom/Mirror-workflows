37a81ba594c2b848b8ecec805527ee8766f1ed14
Andrew Dolgov <noreply@fakecake.org>
2021-02-14 19:19:25 +0300

    SingleUseDialog: destroy existing widget with same id on create

--- js/SingleUseDialog.js
+++ js/SingleUseDialog.js
@@ -1,6 +1,17 @@
+/* eslint-disable prefer-rest-params */
 /* global dijit, define */
 define(["dojo/_base/declare", "dijit/Dialog"], function (declare) {
 	return declare("fox.SingleUseDialog", dijit.Dialog, {
+      create: function(params) {
+            const extant = dijit.byId(params.id);
+
+            if (extant) {
+                  console.warn('SingleUseDialog: destroying existing widget:', params.id, '=', extant)
+                  extant.destroyRecursive();
+            }
+
+            return this.inherited(arguments);
+      },
       onHide: function() {
          this.destroyRecursive();
       }

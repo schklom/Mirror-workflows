053b262aa7d10f2e3c7352d8995a1d1bc1c760e8
Andrew Dolgov <noreply@fakecake.org>
2021-02-19 20:28:15 +0300

    rename public.php/cached_url to cached

--- classes/handler/public.php
+++ classes/handler/public.php
@@ -737,7 +737,7 @@ class Handler_Public extends Handler {
 		<?php
 	}
 
-	function cached_url() {
+	function cached() {
 		list ($cache_dir, $filename) = explode("/", $_GET["file"], 2);
 
 		// we do not allow files with extensions at the moment

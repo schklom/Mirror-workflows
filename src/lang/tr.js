6415564ccfae492cde8ecd0e7707ad52afff04f7
Cadence Ember <cadence@disroot.org>
2021-03-23 23:21:13 +1300

    Update Turkish translation
    Contributed by Oğuz Ersen <oguzersen@protonmail.com>

--- src/lang/tr.js
+++ src/lang/tr.js
@@ -15,15 +15,15 @@ if (!constants.language_dev) Object.assign(data, require("./en.js"))
 	data.about_bibliogram_header = "Bibliogram Hakkında"
 	data.pug_about_bibliogram_content = pug(`
 		p.
-			Bibliogram, Instagram'ın genel profil görünümlerinden veri alan ve daha hızlı yüklenen, indirilebilir resimler veren, 
-			reklamları ortadan kaldıran, RSS beslemeleri oluşturan ve sizi kaydolmaya teşvik etmeyen daha arkadaşça 
+			Bibliogram, Instagram'ın genel profil görünümlerinden veri alan ve daha hızlı yüklenen, indirilebilir resimler veren,
+			reklamları ortadan kaldıran, RSS beslemeleri oluşturan ve sizi kaydolmaya teşvik etmeyen daha arkadaşça
 			bir sayfaya yerleştiren bir web sitesidir. #[a(href=(link_to_featured_profiles ? "#featured-profiles" : "/u/instagram")).example-link Bir örneğe bakın.]
 		p.
 			Bibliogram, özel profilleri anonim olarak paylaşmanıza, beğenmenize, yorum yapmanıza, takip etmenize veya görüntülemenize izin #[em vermez].
 			Silinen gönderileri korumaz.
 	`)
 	data.about_this_instance_header = "Bu örnek hakkında"
-	data.onion_site_available = "Onion sitesi mevcut"
+	data.onion_site_available = "Onion sitesi var"
 	data.t_settings = "Ayarlar"
 	data.t_privacy_policy = "Gizlilik Politikası"
 	data.has_not_written_privacy_policy = "Sahibi gizlilik politikası yazmadı"
@@ -46,11 +46,11 @@ if (!constants.language_dev) Object.assign(data, require("./en.js"))
 	data.verified_badge_title = "Doğrulandı"
 	data.verified_badge_alt = "Doğrulandı."
 	data.post_counter_label = "gönderiler"
-	data.outgoing_follows_counter_label = "Takip etme"
-	data.incoming_follows_counter_label = "Bunu takiben"
+	data.outgoing_follows_counter_label = "Takip edilen"
+	data.incoming_follows_counter_label = "Takip eden"
 	data.quota_left = "Kalan kota:"
-	data.t_home = "Anasayfa"
-	data.tab_timeline = "Zaman çizelgesi"
+	data.t_home = "Ana Sayfa"
+	data.tab_timeline = "Zaman Çizelgesi"
 	data.tab_igtv = "IGTV"
 	data.next_page_button = "Sonraki Sayfa"
 	data.next_page_button_loading = "Yükleniyor..."
@@ -59,20 +59,20 @@ if (!constants.language_dev) Object.assign(data, require("./en.js"))
 	data.no_more_posts_notice = "Başka gönderi yok."
 	data.fn_page_divider = number => `Sayfa ${number}`
 	data.pug_post_timestamp = pug(`
-		| Yayınlanan #[time(datetime=post.date.toISOString() data-local-date)= post.getDisplayDate()].
+		| Gönderi tarihi: #[time(datetime=post.date.toISOString() data-local-date)= post.getDisplayDate()].
 	`)
 	// settings
-	data.t_features = "Özellikleri"
+	data.t_features = "Özellikler"
 	data.t_language = "Dil"
 	data.save_data = "Veri kaydet"
 	data.t_automatic = "Otomatik"
 	data.t_off = "Kapalı"
-	data.lazy_load = "Tembel yük"
+	data.lazy_load = "Tembel yükleme"
 	data.t_full = "Tam"
-	data.rewrite_youtube = "YouTube alanını yeniden yazın"
+	data.rewrite_youtube = "YouTube alanını yeniden yaz"
 	data.rewrite_twitter = "Twitter alanını yeniden yaz"
 	data.remove_trailing_hashtags = "Sondaki hashtagleri gizle"
-	data.t_hide = "Saklamak"
+	data.t_hide = "Gizle"
 	data.link_hashtags = "Tıklanabilir hashtagler"
 	data.t_clickable = "Tıklanabilir"
 	data.show_comments = "Yorumları görüntüle"
@@ -82,7 +82,7 @@ if (!constants.language_dev) Object.assign(data, require("./en.js"))
 	data.infinite_scroll = "Sonsuz kaydırma"
 	data.t_normal = "Normal"
 	data.t_eager = "İstekli"
-	data.t_manual = "Manuel"
+	data.t_manual = "El ile"
 	data.t_appearance = "Görünüm"
 	data.t_theme = "Tema"
 	data.display_top_nav = "Üst çubuğu göster"
@@ -93,14 +93,14 @@ if (!constants.language_dev) Object.assign(data, require("./en.js"))
 	data.four_columns = "4 sütun"
 	data.six_columns = "6 sütun"
 	data.caption_side = "Başlık tarafı"
-	data.left_caption = "Sol (Bibliyogram)"
-	data.right_caption = "Doğru (Instagram)"
+	data.left_caption = "Sol (Bibliogram)"
+	data.right_caption = "Sağ (Instagram)"
 	data.display_alt_text = "Alternatif metni satır içinde görüntüle"
 	data.t_return = "Geri dön"
-	data.t_save = "Kayıt etmek"
+	data.t_save = "Kaydet"
 	data.save_and_return = "Kaydet ve geri dön"
 	data.pug_restore_sync_settings = pug(`
-		| Kaydedilen ayarları şu şekilde geri yükleyebilir ve senkronize edebilirsiniz: #[a(href="/applysettings/"+token)#restore-link bu bağlantıya yer işareti koyma.]
+		| Kaydedilen ayarları #[a(href="/applysettings/"+token)#restore-link bu bağlantıyı yer imlerine ekleyerek] geri yükleyebilir ve eşzamanlayabilirsiniz.
 	`)
 	data.settings_saved = "Kaydedildi."
 

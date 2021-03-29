5bb15abf4c6b9f1aa6561e5975fbc25f011cf184
Nulide <Nulide@tutanota.de>
2021-03-29 18:42:26 +0200

    Add Map on index.html

--- src/web/logic.js
+++ src/web/logic.js
@@ -1,10 +1,27 @@
-var mapOptions = {
-    center: [17.385044, 78.486671],
-    zoom: 14
-    }
-    
-    var map = new L.map("map", mapOptions);
-    
-    var layer = new     L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
-    
-    map.addLayer(layer);
+var map, markers;
+
+function init() {
+    map = new OpenLayers.Map("map");
+    map.addLayer(new OpenLayers.Layer.OSM());
+    map.setCenter(new OpenLayers.LonLat(13.41,52.52) // Center of the map
+        .transform(
+        new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
+            new OpenLayers.Projection("EPSG:900913") // to Spherical Mercator Projection
+          ), 15 // Zoom level
+      );
+    markers = new OpenLayers.Layer.Markers( "Markers" );
+    map.addLayer(markers);
+}
+
+function locate(){
+    var lonLat = new OpenLayers.LonLat( -0.1279688 ,51.5077286 )
+          .transform(
+            new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
+            map.getProjectionObject() // to Spherical Mercator Projection
+          );
+
+    var zoom=16;
+    markers.clearMarkers();
+    markers.addMarker(new OpenLayers.Marker(lonLat));
+    map.setCenter (lonLat, zoom);
+}

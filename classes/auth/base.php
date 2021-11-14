afdb4b00729c8589e99e4ba27981450d1433d9b2
Andrew Dolgov <noreply@fakecake.org>
2021-11-14 20:26:05 +0300

    set phpdoc annotations for auth_base

--- classes/auth/base.php
+++ classes/auth/base.php
@@ -12,8 +12,14 @@ abstract class Auth_Base extends Plugin implements IAuthModule {
 		return $this->authenticate($login, $password, $service);
 	}
 
-	// Auto-creates specified user if allowed by system configuration
-	// Can be used instead of find_user_by_login() by external auth modules
+	/** Auto-creates specified user if allowed by system configuration.
+	 * Can be used instead of find_user_by_login() by external auth modules
+	 * @param string $login
+	 * @param string|false $password
+	 * @return null|int
+	 * @throws Exception
+	 * @throws PDOException
+	 */
 	function auto_create_user(string $login, $password = false) {
 		if ($login && Config::get(Config::AUTH_AUTO_CREATE)) {
 			$user_id = UserHelper::find_user_by_login($login);
@@ -41,7 +47,12 @@ abstract class Auth_Base extends Plugin implements IAuthModule {
 		return UserHelper::find_user_by_login($login);
 	}
 
-	// @deprecated
+
+	/** replaced with UserHelper::find_user_by_login()
+	 * @param string $login
+	 * @return null|int
+	 * @deprecated
+	 */
 	function find_user_by_login(string $login) {
 		return UserHelper::find_user_by_login($login);
 	}

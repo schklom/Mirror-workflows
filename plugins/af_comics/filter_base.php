d17b79311e9c80576a8bf392b9d1dbee7fa8fbdc
Andrew Dolgov <noreply@fakecake.org>
2021-11-14 20:33:37 +0300

    set missing annotations in af_comics

--- plugins/af_comics/filter_base.php
+++ plugins/af_comics/filter_base.php
@@ -1,20 +1,38 @@
 <?php
 abstract class Af_ComicFilter {
+	/** @return array<string> */
 	public abstract function supported();
+
+	/**
+	 * @param array<string,mixed> $article
+	 * @return bool
+	 */
 	public abstract function process(&$article);
 
 	public function __construct(/*PluginHost $host*/) {
 
 	}
 
+	/**
+	 * @param string $url
+	 * @return string|false
+	 */
 	public function on_subscribe($url) {
 		return false;
 	}
 
+	/**
+	 * @param string $url
+	 * @return array{"title": string, "site_url": string}|false
+	 */
 	public function on_basic_info($url) {
 		return false;
 	}
 
+	/**
+	 * @param string $url
+	 * @return string|false
+	 */
 	public function on_fetch($url) {
 		return false;
 	}

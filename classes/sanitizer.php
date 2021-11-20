e7111e4f1422095afcafc92703c4d10dc3358014
Andrew Dolgov <noreply@fakecake.org>
2021-11-20 14:02:37 +0300

    sanitize: make force_remove_images nullable

--- classes/sanitizer.php
+++ classes/sanitizer.php
@@ -62,7 +62,7 @@ class Sanitizer {
 	 *
 	 * @return false|string The HTML, or false if an error occurred.
 	 */
-	public static function sanitize(string $str, bool $force_remove_images = false, int $owner = null, string $site_url = null, array $highlight_words = null, int $article_id = null) {
+	public static function sanitize(string $str, ?bool $force_remove_images = false, int $owner = null, string $site_url = null, array $highlight_words = null, int $article_id = null) {
 
 		if (!$owner && isset($_SESSION["uid"]))
 			$owner = $_SESSION["uid"];

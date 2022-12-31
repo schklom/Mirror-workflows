0e38936034afcdd5eaf41fb1723f87f83249648a
pluja <codeberg@r3d.red>
2022-12-31 08:56:05 +0100

    Minor changes

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -33,11 +33,15 @@
     }
   });
 
+  function _(el) {
+    return document.getElementById(el);
+  }
+
   function handleFileUpload () {
-      if(allowFiles == "true" && fileSelectInput){
-        fileName = fileSelectInput.files.item(0).name;
-        audioAvailable = true;
-      }
+    if(allowFiles == "true" && fileSelectInput){
+      fileName = fileSelectInput.files.item(0).name;
+      audioAvailable = true;
+    }
   };
 
   function renderError(message) {
@@ -125,7 +129,7 @@
         headers: {
           'Content-Type': `mutlipart/form-data;`,
           "Access-Control-Allow-Origin": `${apiHost}`
-        },
+        }       
       });
       processing = false
       transcriptionResultText = response.data.result;
@@ -210,7 +214,7 @@
               <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
           </svg>
           <span>File</span>
-          <input accept="audio/*, video/*" id="fileSelect" bind:this={fileSelectInput} on:change={handleFileUpload} type='file' class="hidden" />
+          <input accept="audio/*, video/*" name="fileSelect" id="fileSelect" bind:this={fileSelectInput} on:change={handleFileUpload} on:loadstart={handleFileUpload} type='file' class="hidden" />
       </label>
       {/if}
       {/if}

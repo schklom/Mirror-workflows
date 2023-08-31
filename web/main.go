067a1c40e32d980fd7c3241e10354bd027e1538a
ManeraKai <manerakai@protonmail.com>
2023-08-31 07:58:02 +0300

    Implemented Cookies

--- web/main.go
+++ web/main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"net/http"
 	"net/url"
+	"time"
 
 	"codeberg.org/SimpleWeb/SimplyTranslate/engines"
 	"github.com/gofiber/fiber/v2"
@@ -14,6 +15,81 @@ func main() {
 	engine := html.New("./views", ".html")
 	app := fiber.New(fiber.Config{
 		Views: engine,
+	})
+
+	app.All("/", func(c *fiber.Ctx) error {
+		engine := c.Query("engine")
+		if _, ok := engines.Engines[engine]; !ok || engine == "" {
+			engine = "google"
+		}
+		targetLanguages, err := engines.Engines[engine].TargetLanguages()
+		if err != nil {
+			return c.SendStatus(500)
+		}
+		sourceLanguages, err := engines.Engines[engine].SourceLanguages()
+		if err != nil {
+			return c.SendStatus(500)
+		}
+		originalText := ""
+		translatedText := ""
+		from := ""
+		to := ""
+		ttsFrom := ""
+		ttsTo := ""
+
+		if c.Method() == "POST" {
+			from = c.FormValue("from")
+			to = c.FormValue("to")
+			originalText = c.FormValue("text")
+			if result, err := engines.Engines[engine].Translate(originalText, from, to); err != nil {
+				return c.SendStatus(500)
+			} else {
+				translatedText = result.TranslatedText
+			}
+
+			ttsFromURL, _ := url.Parse("api/tts")
+			fromQuery := url.Values{}
+			fromQuery.Add("lang", from)
+			fromQuery.Add("text", originalText)
+			ttsFromURL.RawQuery = fromQuery.Encode()
+			ttsFrom = ttsFromURL.String()
+
+			ttsToURL, _ := url.Parse("api/tts")
+			toQuery := url.Values{}
+			toQuery.Add("lang", to)
+			toQuery.Add("text", translatedText)
+			ttsToURL.RawQuery = toQuery.Encode()
+			ttsTo = ttsToURL.String()
+
+			fromCookie := new(fiber.Cookie)
+			fromCookie.Name = "from"
+			fromCookie.Value = from
+			fromCookie.Expires = time.Now().Add(24 * time.Hour * 365)
+			c.Cookie(fromCookie)
+
+			toCookie := new(fiber.Cookie)
+			toCookie.Name = "to"
+			toCookie.Value = to
+			toCookie.Expires = time.Now().Add(24 * time.Hour * 365)
+			c.Cookie(toCookie)
+
+		} else if c.Method() == "GET" {
+			from = c.Cookies("from")
+			to = c.Cookies("to")
+		} else {
+			return c.SendStatus(400)
+		}
+		return c.Render("index", fiber.Map{
+			"Engine":          engine,
+			"SourceLanguages": targetLanguages,
+			"TargetLanguages": sourceLanguages,
+			"OriginalText":    originalText,
+			"TranslatedText":  translatedText,
+			"From":            from,
+			"To":              to,
+			"TtsFrom":         ttsFrom,
+			"TtsTo":           ttsTo,
+		})
 	})
 
 	app.All("/api/translate", func(c *fiber.Ctx) error {
@@ -71,69 +147,6 @@ func main() {
 		}
 	})
 
-	app.All("/", func(c *fiber.Ctx) error {
-		engine := c.Query("engine")
-		if _, ok := engines.Engines[engine]; !ok || engine == "" {
-			engine = "google"
-		}
-		targetLanguages, err := engines.Engines[engine].TargetLanguages()
-		if err != nil {
-			return c.SendStatus(500)
-		}
-		sourceLanguages, err := engines.Engines[engine].SourceLanguages()
-		if err != nil {
-			return c.SendStatus(500)
-		}
-		originalText := ""
-		translatedText := ""
-		from := ""
-		to := ""
-		ttsFrom := ""
-		ttsTo := ""
-
-		if c.Method() == "POST" {
-			from =
-				c.FormValue("from")
-			to = c.FormValue("to")
-			originalText = c.FormValue("text")
-			if result, err := engines.Engines[engine].Translate(originalText, from, to); err != nil {
-				return c.SendStatus(500)
-			} else {
-				translatedText = result.TranslatedText
-			}
-
-			ttsFromURL, _ := url.Parse("api/tts")
-
-			fromQuery := url.Values{}
-			fromQuery.Add("lang", from)
-			fromQuery.Add("text", originalText)
-			ttsFromURL.RawQuery = fromQuery.Encode()
-			ttsFrom = ttsFromURL.String()
-
-			ttsToURL, _ := url.Parse("api/tts")
-
-			toQuery := url.Values{}
-			toQuery.Add("lang", to)
-			toQuery.Add("text", translatedText)
-			ttsToURL.RawQuery = toQuery.Encode()
-			ttsTo = ttsToURL.String()
-
-		}
-		return c.Render("index", fiber.Map{
-			"Engine":          engine,
-			"SourceLanguages": targetLanguages,
-			"TargetLanguages": sourceLanguages,
-			"OriginalText":    originalText,
-			"TranslatedText":  translatedText,
-			"From":            from,
-			"To":              to,
-			"TtsFrom":         ttsFrom,
-			"TtsTo":           ttsTo,
-		})
-	})
-
-	app.Static("/static", "./static")
-
 	app.Get("/api/tts", func(c *fiber.Ctx) error {
 		engine := c.Query("engine")
 		if _, ok := engines.Engines[engine]; !ok || engine == "" {
@@ -163,7 +176,10 @@ func main() {
 				return c.Send(buf.Bytes())
 			}
 		}
+
 	})
+
+	app.Static("/static", "./static")
 
 	app.Listen(":3000")
 }

752c692170a828a4ca6dd6c16070940670da6c8e
Andrew Dolgov <fox@fakecake.org>
2023-10-22 12:46:39 +0300

    use CI_COMMIT_SHORT_SHA for selenium test mock

--- tests/selenium/test.py
+++ tests/selenium/test.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 
+import os
 from selenium import webdriver
+
+CI_COMMIT_SHORT_SHA = os.getenv("CI_COMMIT_SHORT_SHA")
+
+if not CI_COMMIT_SHORT_SHA:
+    print("CI_COMMIT_SHORT_SHA env var should be defined")
+    exit(1)
 
 options = webdriver.ChromeOptions()
 
@@ -9,7 +16,10 @@ driver = webdriver.Remote(
 	options=options
 )
 
-driver.get("http://tt-rss-latest-app.gitlab-fakecake.svc.cluster.local/tt-rss")
+app_url = f"http://tt-rss-{CI_COMMIT_SHORT_SHA}-app.gitlab-fakecake.svc.cluster.local/tt-rss"
+
+print(f"base url = {app_url}")
+
+driver.get(app_url)
 print(driver.page_source)
 driver.quit()
-

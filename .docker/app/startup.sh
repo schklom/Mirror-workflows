ccb4a4d337dec37464dc91ab4be991da17c0db0d
Andrew Dolgov <fox@fakecake.org>
2023-10-22 10:24:14 +0300

    fix previous

--- .docker/app/startup.sh
+++ .docker/app/startup.sh
@@ -52,8 +52,7 @@ else
 	echo "warning: working copy in $DST_DIR won't be updated, make sure you know what you're doing."
 fi
 
-for d in cache lock feed-icons plugins.local
-		themes.local templates.local cache/export cache/feeds cache/images cache/upload; do
+for d in cache lock feed-icons plugins.local themes.local templates.local cache/export cache/feeds cache/images cache/upload; do
 	sudo -u app mkdir -p $DST_DIR/$d
 done
 

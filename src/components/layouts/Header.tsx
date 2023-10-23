d6b3bf21c06d614dec71d8ae1cb7ea44daa8422d
ThePenguinDev <git.thepenguindev.zm2aj@aleeas.com>
2023-10-23 14:19:10 -0400

    feat: add redirect to instagram

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -3,12 +3,21 @@ import {
 	IconInfoCircle,
 	IconRss,
 	IconSettings,
+	IconBrandInstagram,
 } from "@tabler/icons-react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 
 export function Header() {
 	const { asPath, query } = useRouter();
+	const originalUrl = query.tag
+		? `https://instagram.com/explorer/tag/${query.tag}`
+		: `https://instagram.com${asPath}`;
+	const redirectToIG = asPath.endsWith("/stories")
+		? false
+		: query.q
+		? false
+		: true;
 
 	return (
 		<header className="flex h-16 items-center justify-between bg-[#69463d] px-2 text-white shadow-sm shadow-slate-500">
@@ -21,6 +30,17 @@ export function Header() {
 						<li>
 							<Link href={`${asPath}/rss`} target="_blank">
 								<IconRss className="h-6 w-6" />
+							</Link>
+						</li>
+					)}
+					{redirectToIG && (
+						<li>
+							<Link
+								href={originalUrl}
+								referrerPolicy="no-referrer"
+								target="_blank"
+							>
+								<IconBrandInstagram className="h-6 w-6" />
 							</Link>
 						</li>
 					)}

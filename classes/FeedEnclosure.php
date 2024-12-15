62a6191f04e37cc9b40fcf3e713563ed51722e95
wn_ <invalid@email.com>
2024-12-15 13:43:49 +0000

    Deal with FeedEnclosure property accessed before initialization.

--- classes/FeedEnclosure.php
+++ classes/FeedEnclosure.php
@@ -1,9 +1,9 @@
 <?php
 class FeedEnclosure {
-	public string $link;
-	public string $type;
-	public string $length;
-	public string $title;
-	public string $height;
-	public string $width;
+	public string $link = '';
+	public string $type = '';
+	public string $length = '';
+	public string $title = '';
+	public string $height = '';
+	public string $width = '';
 }

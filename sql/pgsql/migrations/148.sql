597971f2382c273106b6205b35f7810a4ed78476
Andrew Dolgov <fox@fakecake.org>
2025-04-08 09:48:44 +0300

    we no longer directly modify schema_version in migrations

--- sql/pgsql/migrations/148.sql
+++ sql/pgsql/migrations/148.sql
@@ -3,5 +3,3 @@ alter table ttrss_feeds add column auth_pass text;
 update ttrss_feeds set auth_pass = auth_pass_old;
 alter table ttrss_feeds alter column auth_pass set not null;
 alter table ttrss_feeds drop column auth_pass_old;
-
-update ttrss_version set schema_version = 148;

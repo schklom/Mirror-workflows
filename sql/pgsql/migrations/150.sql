5256edd484d6d3efdd870fd04d09c289e2d23c61
Andrew Dolgov <fox@fakecake.org>
2025-05-02 21:51:07 +0300

    schema - spaces to tabs

--- sql/pgsql/migrations/150.sql
+++ sql/pgsql/migrations/150.sql
@@ -1,6 +1,6 @@
 create table ttrss_scheduled_tasks(
-   id serial not null primary key,
-   task_name varchar(250) unique not null,
-   last_duration integer not null,
-   last_rc integer not null,
-   last_run timestamp not null default NOW());
+	id serial not null primary key,
+	task_name varchar(250) unique not null,
+	last_duration integer not null,
+	last_rc integer not null,
+	last_run timestamp not null default NOW());

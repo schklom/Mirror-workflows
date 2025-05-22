33047a86bab3c15470829431015df1c8a59d98f6
Andrew Dolgov <fox@fakecake.org>
2025-05-22 11:45:08 +0300

    try adding selenium page timeout

--- tests/integration/selenium_test.py
+++ tests/integration/selenium_test.py
@@ -18,6 +18,8 @@ class SeleniumTest(unittest.TestCase):
         self.driver = webdriver.Remote(command_executor=SELENIUM_GRID_ENDPOINT, options=webdriver.ChromeOptions())
         self.base_url = f"http://tt-rss-{CI_COMMIT_SHORT_SHA}-app.{K8S_NAMESPACE}.svc.cluster.local/tt-rss"
 
+        self.driver.set_page_load_timeout(10)
+
     def tearDown(self):
         self.driver.quit()
 

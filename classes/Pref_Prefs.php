681d04032af96b01131f830566f8e6ad3561b06c
supahgreg <supahgreg@users.noreply.github.com>
2025-11-16 16:39:46 +0000

    Minor cleanup in 'Pref_Prefs::_get_updated_plugins()'

--- classes/Pref_Prefs.php
+++ classes/Pref_Prefs.php
@@ -1069,9 +1069,7 @@ class Pref_Prefs extends Handler_Protected {
 			}
 		}
 
-		$rv = array_values(array_filter($rv, fn(?array $item): bool => $item['rv'] !== null && $item['rv']['need_update']));
-
-		return $rv;
+		return array_values(array_filter($rv, fn(array $item): bool => $item['rv'] !== null && $item['rv']['need_update']));
 	}
 
 	/**

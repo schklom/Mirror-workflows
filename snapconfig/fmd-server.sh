6b5da564c5228ff95ee5fa2d5bfabe581f504bd7
Nulide <nulide@posteo.de>
2025-11-19 15:17:42 +0100

    Put bash variables in brackets

--- snapconfig/fmd-server.sh
+++ snapconfig/fmd-server.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
-SRC="$SNAP/config.example.yml"
-DST="$SNAP_USER_COMMON/config.yml"
+SRC="${SNAP}/config.example.yml"
+DST="${SNAP_COMMON}/config.yml"
 
 # Copy config only if not present
-if [[ ! -f "$DST" ]]; then
+if [[ ! -f "${DST}" ]]; then
     # Create directory if needed
-    DB_DST="$SNAP_USER_COMMON/db"
-    mkdir -p "$DB_DST"
+    DB_DST="${SNAP_COMMON}/db"
+    mkdir -p "${DB_DST}"
 
-    cp "$SRC" "$DST"
-    chmod 600 "$DST"
+    cp "${SRC}" "${DST}"
     # Set Database directory
-    sed -i "s|DatabaseDir: \"\"|DatabaseDir: \"$DB_DST\"|g" $DST
+    sed -i "s|DatabaseDir: \"\"|DatabaseDir: \"${DB_DST}\"|g" ${DST}
 fi
 
 # Start your actual app
-exec "$SNAP/bin/fmd-server" "$@"
+exec "${SNAP}/bin/fmd-server" "$@"

ea4e0b223c1fbb86ef43178ce12f9612bee76b3c
Thore Goebel <git@thore.io>
2025-12-29 21:00:21 +0100

    Explain config path ordering

--- config/config.go
+++ config/config.go
@@ -102,8 +102,10 @@ func ReadConfigFile(config *viper.Viper, configFilePath string) {
 		// DO NOT fall back to these locations.
 		// Better to fail reading the file below to let the admin know something is wrong,
 		// than silently using a different file (which can be hard to debug).
+		//
+		// Order matters! Config paths that are added first take priority.
 		snapPath, isSnap := os.LookupEnv("SNAP_COMMON")
-		if isSnap {		
+		if isSnap {
 			config.AddConfigPath(snapPath)
 		}
 		config.AddConfigPath("/etc/fmd-server/")

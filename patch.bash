c00aee520a95d30ed7f5487d4d5bcc23876ce54d
schklom <23263086+schklom@users.noreply.github.com>
2026-03-01 11:39:27 +0000

    Printing update

--- patch.bash
+++ patch.bash
@@ -3,5 +3,10 @@
 # https://github.com/shenxn/protonmail-bridge-docker/pull/139/changes
 # Add libfido2-dev libcbor-dev to first build stage
 # Add libfido2-1 to second stage
+cp build/Dockerfile build/Dockerfile.bak
 sed -i "s/build-essential/build-essential libfido2-dev libcbor-dev/" build/Dockerfile
 sed -i "s/ca-certificates/ca-certificates libfido2-1/" build/Dockerfile
+
+echo "Changes in build/Dockerfile"
+diff build/Dockerfile build/Dockerfile.bak
+rm build/Dockerfile.bak

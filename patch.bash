2aef758cb292af229e25a5022e8101060f1513de
schklom <23263086+schklom@users.noreply.github.com>
2026-03-01 11:29:16 +0000

    Update patch.bash to modify Dockerfile for dependencies

--- patch.bash
+++ patch.bash
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # https://github.com/shenxn/protonmail-bridge-docker/pull/139/changes
-# Add libfido2-dev libcbor-dev to first build stage, and libfido2-1 to second stage
-sed -E "s/build-essential/build-essential libfido2-dev libcbor-dev/" build/Dockerfile
-sed -E "s/ca-certificates/ca-certificates libfido2-1/" build/Dockerfile
+# Add libfido2-dev libcbor-dev to first build stage
+# Add libfido2-1 to second stage
+sed -i "s/build-essential/build-essential libfido2-dev libcbor-dev/" build/Dockerfile
+sed -i "s/ca-certificates/ca-certificates libfido2-1/" build/Dockerfile

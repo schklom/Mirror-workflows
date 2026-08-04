7c755e8b73e72cb960a5aef117f4c51c7bb596be
Derek Schrock <dereks@lifeofadishwasher.com>
2026-08-03 22:12:24 -0400

    Fix deprecated warning with parse_url within get_self_url (#411)

--- classes/Config.php
+++ classes/Config.php
@@ -553,7 +553,7 @@ class Config {
 		} else {
 			$proto = self::is_server_https() ? 'https' : 'http';
 
-			$self_url_path = $proto . '://' . $_SERVER["HTTP_HOST"] . parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+			$self_url_path = $proto . '://' . $_SERVER["HTTP_HOST"] . parse_url($_SERVER["REQUEST_URI"] ?? '', PHP_URL_PATH);
 			$self_url_path = preg_replace("/(\/api\/{1,})?(\w+\.php)?(\?.*$)?$/", "", $self_url_path);
 			$self_url_path = preg_replace("/(\/plugins(.local)?)\/.{1,}$/", "", $self_url_path);
 		}

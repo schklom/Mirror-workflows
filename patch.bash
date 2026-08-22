0041b8a64112ae04251c2ecb85976077fb83d0c9
schklom <23263086+schklom@users.noreply.github.com>
2026-08-22 16:49:52 +0000

    Combine mkdir commands into a single RUN instruction

--- patch.bash
+++ patch.bash
@@ -3,8 +3,7 @@
 cp Dockerfile Dockerfile.bak
 echo "RUN apk add --no-cache git" >> Dockerfile
 echo "RUN git clone --depth 1 https://github.com/hasaneyldrm/exercises-dataset ds" >> Dockerfile
-echo "mkdir -p /usr/share/nginx/html/" >> Dockerfile
-echo "mkdir -p /usr/share/nginx/gif/" >> Dockerfile
+echo "RUN mkdir -p /usr/share/nginx/html /usr/share/nginx/gif" >> Dockerfile
 echo "RUN mv /ds/images/*.jpg /usr/share/nginx/html/" >> Dockerfile
 echo "RUN mv /ds/videos/*.gif /usr/share/nginx/gif/" >> Dockerfile
 echo "RUN rm -rf /ds" >> Dockerfile

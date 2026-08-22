6e3fe15ee071b77e6d287fc9621e3238520577bf
schklom <23263086+schklom@users.noreply.github.com>
2026-08-22 20:26:05 +0000

    cat EOF is cleaner and fixes quotation bugs

--- patch.bash
+++ patch.bash
@@ -3,19 +3,23 @@
 cp Dockerfile Dockerfile.bak
 
 # Copy data files into the image to avoid forcing the user to do it
-echo "RUN apk add --no-cache git" >> Dockerfile
-echo "RUN git clone --depth 1 https://github.com/hasaneyldrm/exercises-dataset ds" >> Dockerfile
-echo "RUN mkdir -p /usr/share/nginx/html /usr/share/nginx/gif" >> Dockerfile
-echo "RUN mv /ds/images/*.jpg /usr/share/nginx/html/" >> Dockerfile
-echo "RUN mv /ds/videos/*.gif /usr/share/nginx/gif/" >> Dockerfile
-echo "RUN rm -rf /ds" >> Dockerfile
+cat << 'EOF' >> Dockerfile
+RUN apk add --no-cache git
+RUN git clone --depth 1 https://github.com/hasaneyldrm/exercises-dataset ds
+RUN mkdir -p /usr/share/nginx/html /usr/share/nginx/gif
+RUN mv /ds/images/*.jpg /usr/share/nginx/html/
+RUN mv /ds/videos/*.gif /usr/share/nginx/gif/
+RUN rm -rf /ds" >> Dockerfile
+EOF
 
 # Allow API_HOST and API_PORT environment variables
-echo 'RUN sed -i \'s|proxy_pass http://api:3000|proxypass proxy_pass http://${API_HOST}:${API_PORT}|\' "/etc/nginx/conf.d/default.conf"' >> Dockerfile
-echo 'RUN mv "/etc/nginx/conf.d/default.conf" "/etc/nginx/templates/default.conf.template"' >> Dockerfile
-echo "ENV API_HOST=api" >> Dockerfile
-echo "ENV API_HOST=3000" >> Dockerfile
-echo 'ENV NGINX_ENVSUBST_FILTER="API_HOST API_PORT"'
+cat << 'EOF' >> Dockerfile
+RUN sed -i 's|proxy_pass http://api:3000|proxy_pass http://${API_HOST}:${API_PORT}|' "/etc/nginx/conf.d/default.conf"
+RUN mv "/etc/nginx/conf.d/default.conf" "/etc/nginx/templates/default.conf.template"
+ENV API_HOST=api
+ENV API_HOST=3000
+ENV NGINX_ENVSUBST_FILTER="API_HOST API_PORT"
+EOF
 
 echo "Changes in Dockerfile"
 diff Dockerfile.bak Dockerfile || true

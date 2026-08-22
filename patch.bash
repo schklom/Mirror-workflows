8a359115eae1ed93ced82a4d26223a6551659bf8
schklom <23263086+schklom@users.noreply.github.com>
2026-08-22 13:52:11 +0000

    Update patch.bash

--- patch.bash
+++ patch.bash
@@ -5,9 +5,9 @@ echo "RUN apk add --no-cache git" >> Dockerfile
 echo "RUN git clone --depth 1 https://github.com/hasaneyldrm/exercises-dataset ds" >> Dockerfile
 echo "RUN ls -ahl ds" >> Dockerfile
 echo "RUN pwd" >> Dockerfile
-echo "COPY ds/images/*.jpg /usr/share/nginx/html/" >> Dockerfile
-echo "COPY ds/videos/*.gif /usr/share/nginx/gif/" >> Dockerfile
-echo "RUN rm -rf ds" >> Dockerfile
+echo "COPY /ds/images/*.jpg /usr/share/nginx/html/" >> Dockerfile
+echo "COPY /ds/videos/*.gif /usr/share/nginx/gif/" >> Dockerfile
+echo "RUN rm -rf /ds" >> Dockerfile
 
 echo "Changes in Dockerfile"
 diff Dockerfile.bak Dockerfile

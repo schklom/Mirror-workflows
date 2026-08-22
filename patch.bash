22bec4acc105e30d9ac02270a557ab59171228dd
schklom <23263086+schklom@users.noreply.github.com>
2026-08-22 18:27:17 +0000

    fix diff returning 1

--- patch.bash
+++ patch.bash
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-echo pwd
-pwd
-
-echo "ls -ahl"
-ls -ahl
-
-echo "ls -ahl patch"
-ls -ahl patch
-
 cp Dockerfile Dockerfile.bak
 echo "RUN apk add --no-cache git" >> Dockerfile
 echo "RUN git clone --depth 1 https://github.com/hasaneyldrm/exercises-dataset ds" >> Dockerfile
@@ -18,9 +9,6 @@ echo "RUN mv /ds/videos/*.gif /usr/share/nginx/gif/" >> Dockerfile
 echo "RUN rm -rf /ds" >> Dockerfile
 
 echo "Changes in Dockerfile"
-diff Dockerfile.bak Dockerfile
+diff Dockerfile.bak Dockerfile || true
 echo "Delete Dockerfile.bak"
 rm -f Dockerfile.bak
-
-echo "FINISHED"
-exit 0

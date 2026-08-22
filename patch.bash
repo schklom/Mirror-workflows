0636568a8e0e696e029bf17833034cca7ef7bf44
schklom <23263086+schklom@users.noreply.github.com>
2026-08-22 13:48:19 +0000

    add debug step
    Updated Dockerfile commands to improve clarity and functionality.

--- patch.bash
+++ patch.bash
@@ -3,7 +3,8 @@
 cp Dockerfile Dockerfile.bak
 echo "RUN apk add --no-cache git" >> Dockerfile
 echo "RUN git clone --depth 1 https://github.com/hasaneyldrm/exercises-dataset ds" >> Dockerfile
-echo "RUN ls -ahlR ds" >> Dockerfile
+echo "RUN ls -ahl ds" >> Dockerfile
+echo "RUN pwd" >> Dockerfile
 echo "COPY ds/images/*.jpg /usr/share/nginx/html/" >> Dockerfile
 echo "COPY ds/videos/*.gif /usr/share/nginx/gif/" >> Dockerfile
 echo "RUN rm -rf ds" >> Dockerfile

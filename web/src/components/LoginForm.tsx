59b79e7353ba50b7f11a26906c65500f05452f20
Thore Goebel <git@thore.io>
2026-08-26 21:39:35 +0200

    frontend: Multiplex betwee APIv1 and v2

--- web/src/components/LoginForm.tsx
+++ web/src/components/LoginForm.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
 import { ApiError, getVersion } from '@/lib/api';
-import { apiService } from '@/lib/apiService';
+import { apiService, updateApiService } from '@/lib/apiService';
 import { hashPasswordForLogin } from '@/lib/crypto';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
@@ -12,6 +12,7 @@ import { PasswordInput } from '@/components/PasswordInput';
 import { Checkbox } from '@/components/Checkbox';
 import { WebCryptoWarningModal } from './modals/WebCryptoWarningModal';
 import { LanguageNativeSelect } from './LanguageNativeSelect';
+import { ApiV2Service } from '@/lib/apiv2';
 
 const SLOW_LOGIN_THRESHOLD_MS = 10_000;
 const SLOW_LOGIN_TOAST_DURATION_MS = 30_000;
@@ -62,7 +63,8 @@ export const LoginForm = () => {
     setLoading(true);
 
     try {
-      const salt = await apiService().getSalt(fmdId);
+      const [salt, protoVersion] = await new ApiV2Service().getSalt(fmdId);
+      updateApiService(protoVersion);
 
       if (!salt) {
         toast.error(t('errors:account_not_found'));
--- web/src/lib/apiService.ts
+++ web/src/lib/apiService.ts
@@ -1,10 +1,11 @@
 import { BaseApiService } from './api';
 import { ApiV1Service } from './apiv1';
-import { CRYPTO_PROTO_V1 } from './crypto';
+import { ApiV2Service } from './apiv2';
+import { CRYPTO_PROTO_V1, CRYPTO_PROTO_V2 } from './crypto';
 
 // This is a separate file to avoid circular imports.
 
-let _apiService: BaseApiService = new ApiV1Service();
+let _apiService: BaseApiService = new ApiV2Service();
 
 /**
  * Gets the current global BaseApiService singleton.
@@ -18,6 +19,9 @@ export function updateApiService(protoVersion: number) {
   switch (protoVersion) {
     case CRYPTO_PROTO_V1:
       _apiService = new ApiV1Service();
+      break;
+    case CRYPTO_PROTO_V2:
+      _apiService = new ApiV2Service();
       break;
     default:
       throw new Error(`unknown protoVersion: ${protoVersion}`);

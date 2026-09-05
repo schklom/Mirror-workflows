ccb016bdc2572d3b17e661ac1c04cee0e7699976
Thore Goebel <git@thore.io>
2026-09-05 22:12:05 +0200

    Initialize metrics with v2 data

--- user/user_metrics.go
+++ user/user_metrics.go
@@ -10,14 +10,20 @@ func initializeUserMetrics(db *FMDDB) {
 	var locationCount int64
 	db.DB.Model(&Location{}).Count(&locationCount)
 	metrics.Locations.Set(float64(locationCount))
+	db.DB.Model(&LocationV2{}).Count(&locationCount)
+	metrics.Locations.Add(float64(locationCount))
 
 	var pictureCount int64
 	db.DB.Model(&Picture{}).Count(&pictureCount)
 	metrics.Pictures.Set(float64(pictureCount))
+	db.DB.Model(&PictureV2{}).Count(&pictureCount)
+	metrics.Pictures.Add(float64(pictureCount))
 
 	var pendingCommandCount int64
 	db.DB.Model(&FMDUser{}).Where("command_to_user IS NOT NULL AND command_to_user <> ''").Count(&pendingCommandCount)
 	metrics.PendingCommands.Set(float64(pendingCommandCount))
+	db.DB.Model(&CommandV2{}).Count(&pendingCommandCount)
+	metrics.PendingCommands.Add(float64(pendingCommandCount))
 
 	var pendingMessageCount int64
 	db.DB.Model(&Message{}).Count(&pendingMessageCount)

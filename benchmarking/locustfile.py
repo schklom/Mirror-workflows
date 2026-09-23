a98bde453d6d509ef5b1fe606ca911e2345277a5
Thore Goebel <git@thore.io>
2026-09-23 21:00:24 +0200

    Format with ruff

--- benchmarking/locustfile.py
+++ benchmarking/locustfile.py
@@ -10,34 +10,32 @@ URL_LOGIN = URL_API + "/account/login"
 URL_LOCATION = URL_API + "/data/location"
 URL_PICTURE = URL_API + "/data/picture"
 
-class FMD(HttpUser):
 
-    #Register the user on FMD-Server
+class FMD(HttpUser):
+    # Register the user on FMD-Server
     def on_start(self):
         self.username = uuid.uuid7().hex
         self.salt = b64("salt")
-        self.pw = b64("pw"+self.username)
-        self.key = b64("key"+self.username)
+        self.pw = b64("pw" + self.username)
+        self.key = b64("key" + self.username)
 
         register_request = {
-          "username": self.username,
-          "salt64": self.salt,
-          "passwordHash64": self.pw,
-          "protoVersion": 2,
-          "encMasterKey64": self.key,
-          "registrationToken": ""
+            "username": self.username,
+            "salt64": self.salt,
+            "passwordHash64": self.pw,
+            "protoVersion": 2,
+            "encMasterKey64": self.key,
+            "registrationToken": "",
         }
-        self.client.post(URL_REGISTER,
-            json=register_request)
+        self.client.post(URL_REGISTER, json=register_request)
 
     def get_access_token(self) -> str | None:
         login_request = {
-          "username": self.username,
-          "passwordHash64": self.pw,
-          "sessionDurationSeconds": 60
+            "username": self.username,
+            "passwordHash64": self.pw,
+            "sessionDurationSeconds": 60,
         }
-        response = self.client.post(URL_LOGIN,
-            json=login_request)
+        response = self.client.post(URL_LOGIN, json=login_request)
         data = response.json()
         if "accessToken" in data:
             return data["accessToken"]
@@ -47,21 +45,22 @@ class FMD(HttpUser):
         token = self.get_access_token()
         if token:
             item = {
-            "items": [
-                {
-                "clientItemIdHex": uuid.uuid7().hex,
-                "unixMillis": int(time()),
-                "ciphertext64": b64(data)
-                }
-            ]
+                "items": [
+                    {
+                        "clientItemIdHex": uuid.uuid7().hex,
+                        "unixMillis": int(time()),
+                        "ciphertext64": b64(data),
+                    }
+                ]
             }
-            self.client.post(url, json=item, headers={"Authorization": "Bearer " + token})
+            self.client.post(
+                url, json=item, headers={"Authorization": "Bearer " + token}
+            )
 
     def get_data(self, url):
         token = self.get_access_token()
         if token:
             self.client.get(url, headers={"Authorization": "Bearer " + token})
-
 
     @task
     def post_location(self):
